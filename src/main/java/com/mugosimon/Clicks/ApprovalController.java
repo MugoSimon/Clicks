58b92effbcb71cf181d19885d0573868f56833fe
mugo simon <mugolastbon@gmail.com>
2023-05-26 11:07:21 +0300

    third commit.... css added and changed button

--- src/main/java/com/mugosimon/Clicks/ApprovalController.java
+++ src/main/java/com/mugosimon/Clicks/ApprovalController.java
@@ -3,9 +3,11 @@ package com.mugosimon.Clicks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@CrossOrigin
 @Controller
 public class ApprovalController {
     private final ApprovalService approvalService;
