80f6624a72fb3ba38bf03267be76e268e3ca04b5
mugo simon <mugolastbon@gmail.com>
2023-05-26 14:05:33 +0300

    #done

--- src/main/java/com/mugosimon/Clicks/ApprovalRepository.java
+++ src/main/java/com/mugosimon/Clicks/ApprovalRepository.java
@@ -1,7 +1,9 @@
 package com.mugosimon.Clicks;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ApprovalRepository
         extends JpaRepository<Approval, Long> {
 }
